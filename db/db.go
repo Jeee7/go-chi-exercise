9a49ec7afafdd77a965559847bf7db0f1f9fb880
Jeee7 <arya.ziddan7@gmail.com>
2025-06-06 21:25:11 +0700

    feat: initial commit

---
+++ db/db.go
@@ -0,0 +1,14 @@
+// package db
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"github.com/jackc/pgx/v5/pgxpool"
+// 	"log"
+// )
+
+// var Pool *pgxpool.Pool
+
+// func ConnectDB () {
+// 	dsn := 
+// }
